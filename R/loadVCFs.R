1c4bbdb0295d672746353ab96a6ea85709eca6ec
Bernat Gel <bernatgel@gmail.com>
2019-10-02 13:34:20 +0200

    Removed dplyr dependency

--- R/loadVCFs.R
+++ R/loadVCFs.R
@@ -63,11 +63,11 @@ loadVCFs <- function(vcf.paths, sample.names = NULL, cnvs.gr,
                      genome = "hg19", verbose = TRUE) {
 
   # Check input
-  assert_that(is.character(vcf.paths))
-  assert_that(is.number(min.total.depth))
-  assert_that(is.numeric(homozygous.range) && length(homozygous.range) == 2)
+  assertthat::assert_that(is.character(vcf.paths))
+  assertthat::assert_that(is.number(min.total.depth))
+  assertthat::assert_that(is.numeric(homozygous.range) && length(homozygous.range) == 2)
   if (!is.null(sample.names))
-    assert_that(length(vcf.paths) == length(sample.names))
+    assertthat::assert_that(length(vcf.paths) == length(sample.names))
 
 
   # Decide where sample names are obtained from
@@ -97,13 +97,13 @@ loadVCFs <- function(vcf.paths, sample.names = NULL, cnvs.gr,
 
       # compress if necessary
       if (!file.exists(vcfFile)){
-        bgzip(originalVcfFile, vcfFile, overwrite = TRUE)  # generate .gz in a temp path
+        Rsamtools::bgzip(originalVcfFile, vcfFile, overwrite = TRUE)  # generate .gz in a temp path
       }
 
       # create tabix file if necessary
       tbiFile <- paste0(vcfFile, ".tbi")
       if (!file.exists(tbiFile))
-        idx <- indexTabix(vcfFile, "vcf")
+        idx <- Rsamtools::indexTabix(vcfFile, "vcf")
     }
 
 
@@ -157,7 +157,6 @@ loadVCFs <- function(vcf.paths, sample.names = NULL, cnvs.gr,
 #' @return Processed \code{vars}
 #'
 #' @import assertthat
-#' @importFrom dplyr between
 #' @importFrom IRanges subsetByOverlaps
 #' @importFrom GenomicRanges mcols
 #'
@@ -165,17 +164,11 @@ auxProcessVariants <- function(vars, cnvGR, heterozygous.range, homozygous.range
 
   # Exclude variants overlaping regions.to.exclude
   if (!is.null(regions.to.exclude)) {
-    vars <- subsetByOverlaps(vars, regions.to.exclude,  type = "any", invert = TRUE)
+    vars <- IRanges::subsetByOverlaps(vars, regions.to.exclude,  type = "any", invert = TRUE)
   }
 
-  # seqinfo(vars) <- seqinfo(cnvGR)
-  # seqinfo(vars) = NULL
-  # vars <- regioneR::filterChromosomes(vars, organism = genome(vars)[1])
-  # # ídem seqinfo(cnvGR)
-
-
   # Subset: only variants on CNV regions
-  vars <- subsetByOverlaps(vars, cnvGR, type = "any")
+  vars <- IRanges::subsetByOverlaps(vars, cnvGR, type = "any")
 
   # Process variants
   if (length(vars) > 0){
@@ -184,9 +177,9 @@ auxProcessVariants <- function(vars, cnvGR, heterozygous.range, homozygous.range
     mcolumns$type <- ""
     for (i in seq_len(nrow(mcolumns))){
       v <- mcolumns[i,]
-      if (between(v$alt.freq, heterozygous.range[1], heterozygous.range[2])) {
+      if (v$alt.freq > heterozygous.range[1] & v$alt.freq < heterozygous.range[2]) {
         mcolumns[i,"type"] <- "ht"
-      } else if (between(v$alt.freq, homozygous.range[1], homozygous.range[2])){
+      } else if (v$alt.freq > homozygous.range[1] & v$alt.freq < homozygous.range[2]){
         mcolumns[i,"type"] <- "hm"
       }
     }
@@ -197,8 +190,8 @@ auxProcessVariants <- function(vars, cnvGR, heterozygous.range, homozygous.range
     # retag as overlap_indel those SNV variants overlapped by an indel. Those variant will no be used in analysis
     snvs <- vars[!vars$indel,]
     indels <- vars[vars$indel,]
-    overlapped <- subsetByOverlaps(snvs, indels)
-    indexes <- which(row.names(mcols(vars)) %in% row.names(mcols(overlapped)))
+    overlapped <- IRanges::subsetByOverlaps(snvs, indels)
+    indexes <- which(row.names(GenomicRanges::mcols(vars)) %in% row.names(GenomicRanges::mcols(overlapped)))
     if (length(indexes) > 1){
       GenomicRanges::mcols(vars[indexes,])$type <- "overlap_indel"
     }
