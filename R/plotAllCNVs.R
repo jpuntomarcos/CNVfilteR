a5645f81ee9572cd28271a98a41a79357ef7c0ea
Jose Marcos Moreno <jmmoreno@ws-es-totoro.igtpscp.local>
2020-11-28 16:47:08 +0100

    Added genome parameter to allow this function to work with different genomes

--- R/plotAllCNVs.R
+++ R/plotAllCNVs.R
@@ -7,6 +7,7 @@
 #' Plots all CNVs defined at \code{cnvs.gr} on a view of horizontal ideograms representing all chromosomes.
 #'
 #' @param cnvs.gr \code{GRanges} containing al CNV definitions returned by \code{filterCNVs} or \code{loadCNVcalls} functions.
+#' @param genome The name of the genome. (Defaults to "hg19")
 #'
 #' @return invisibly returns a \code{karyoplot} object
 #'
@@ -25,15 +26,16 @@
 #' @importFrom methods is
 #' @export plotAllCNVs
 #'
-plotAllCNVs <- function(cnvs.gr){
+plotAllCNVs <- function(cnvs.gr, genome = "hg19"){
 
   assertthat::assert_that(methods::is(cnvs.gr, "GRanges"))
+  assertthat::assert_that(assertthat::is.string(genome))
 
   # Add cn column (required by CopyNumberPlots)
   cnvs.gr <- auxAddCNcolumn(cnvs.gr)
 
   # Plot
-  kp <- karyoploteR::plotKaryotype(plot.type=1)
+  kp <- karyoploteR::plotKaryotype(plot.type=1, genome = genome)
   CopyNumberPlots::plotCopyNumberCalls(kp, cnvs.gr, r1=0.3, cn.colors = CNV_COLORS, label.cex = 0.6)
   graphics::legend("bottomright", legend=c("Deletion CNV", "Duplication CNV"), fill = c(CNV_COLORS[2], CNV_COLORS[4]), ncol=1, cex = 0.7)
 
